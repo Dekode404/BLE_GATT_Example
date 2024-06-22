b8c05befc54bf2b303c82440e1ff62213a3e9ffc
saurabh kadam <ssk3@somaiya.edu>
2024-06-22 17:16:05 +0530

    Add BLE_app_advertise function this used to advertise.

--- main/main.c
+++ main/main.c
@@ -15,10 +15,35 @@
 #include <host/ble_hs.h>                 /* This is ESP lib used for the ble host controller */
 #include <services/gap/ble_svc_gap.h>    /* This is ESP lib used for initiate the ble service */
 
-#define URL_SITE "parasdefence"
+#define DEVICE_NAME "HARTMAN_SIGHT"
+
+void BLE_app_advertise(void);
+
+uint8_t ble_addr_type;
+
+void BLE_app_advertise(void)
+{
+    struct ble_hs_adv_fields fields;
+    memset(&fields, 0, sizeof(fields));
+
+    fields.flags = BLE_HS_ADV_F_DISC_GEN | BLE_HS_ADV_F_DISC_LTD;
+    fields.tx_pwr_lvl_is_present = true;
+    fields.tx_pwr_lvl = BLE_HS_ADV_TX_PWR_LVL_AUTO;
+
+    fields.name = (uint8_t *)ble_svc_gap_device_name();
+    fields.name_len = strlen(ble_svc_gap_device_name());
+    fields.name_is_complete = true;
+
+    ble_gap_adv_set_fields(&fields);
+
+    struct ble_gap_adv_params adv_params = (struct ble_gap_adv_params){0}; /* Advertising parameters */
+    ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, NULL, NULL);
+}
 
 void BLE_app_on_sync(void)
 {
+    ble_hs_id_infer_auto(0, &ble_addr_type);
+    BLE_app_advertise();
 }
 
 void Host_task(void *param)
@@ -31,8 +56,10 @@ void app_main(void)
     ESP_ERROR_CHECK(nvs_flash_init()); /* Initialize NVS flash */
 
     ESP_ERROR_CHECK(esp_nimble_hci_and_controller_init()); /* Initiate the Nimble BLE Hardware control interference and controller */
+    nimble_port_init();                                    /* Initiate the nimble port for the BLE */
 
-    nimble_port_init(); /* Initiate the nimble port for the BLE */
+    ble_svc_gap_device_name_set(DEVICE_NAME);
+    ble_svc_gap_init();
 
     ble_hs_cfg.sync_cb = BLE_app_on_sync; /* Set the synchronization callback */
     nimble_port_freertos_init(Host_task); /* Initialize NimBLE port with FreeRTOS */
