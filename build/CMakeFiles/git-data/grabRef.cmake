41a9e7cda9ea9036768e2e2940344f74d6568908
saurabh kadam <ssk3@somaiya.edu>
2024-06-22 16:41:08 +0530

    Change the BLE HOST mode from default to the Nimble-only

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/project/ESP32/BLE_GATT_Example/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/esp/idf/esp-idf/.git")
+set(GIT_DIR "D:/project/ESP32/BLE_GATT_Example/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
