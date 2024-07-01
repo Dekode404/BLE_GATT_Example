e19fc7ae0181604052e0c1c4f63a0869982e66dd
saurabh kadam <ssk3@somaiya.edu>
2024-07-01 16:33:41 +0530

    Add code for the notification in the battery service

--- main/main.c
+++ main/main.c
@@ -18,16 +18,49 @@
 
 #define DEVICE_NAME "HARTMAN_SIGHT"
 
-void BLE_app_advertise(void);
-
-uint8_t BLE_Addr_Type;
-
 #define DEVICE_INFO_SERVICE 0x180A
 #define MANUFACTURER_NAME 0x2A29
 
 #define DEVICE_BATTERY_SERVICE 0x180F
 #define BATTERY_LEVEL 0x2A19
+#define BATTERY_CLIENT_CONFIG_DESCRIPTOR 0x2902
 #define BATTERY_INFORMATION 0x2BEC
+
+void BLE_app_advertise(void);
+
+uint8_t BLE_Addr_Type;
+
+uint8_t Battery_Level = 100;
+
+uint16_t Battery_level_characteristic_attribute_handler;
+uint16_t connection_handler;
+
+static xTimerHandle Battery_Timer_Handler;
+
+// see https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Descriptors/org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+static uint8_t config[2] = {0x01, 0x00};
+
+static int Battery_Level_Descriptor(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    if (ctxt->op == BLE_GATT_ACCESS_OP_READ_DSC)
+    {
+        os_mbuf_append(ctxt->om, &config, sizeof(config));
+    }
+    else
+    {
+        memcpy(config, ctxt->om->om_data, ctxt->om->om_len);
+    }
+
+    if (config[0] == 0x01)
+    {
+        xTimerStart(Battery_Timer_Handler, 0);
+    }
+    else
+    {
+        xTimerStop(Battery_Timer_Handler, 0);
+    }
+    return 0;
+}
 
 static int Custom_Service(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
@@ -56,20 +89,73 @@ static int Device_Info(uint16_t conn_handle, uint16_t attr_handle, struct ble_ga
     return 0;
 }
 
-static const struct ble_gatt_svc_def GATT_Service[] = {
-    {.type = BLE_GATT_SVC_TYPE_PRIMARY,
-     .uuid = BLE_UUID16_DECLARE(DEVICE_INFO_SERVICE),
-     .characteristics = (struct ble_gatt_chr_def[]){
-         {.uuid = BLE_UUID16_DECLARE(MANUFACTURER_NAME),
-          .flags = BLE_GATT_CHR_F_READ,
-          .access_cb = Device_Info},
-         {0}}},
+static const struct ble_gatt_svc_def GATT_Service[] =
+    {
+        {
+            .type = BLE_GATT_SVC_TYPE_PRIMARY,
+            .uuid = BLE_UUID16_DECLARE(DEVICE_INFO_SERVICE),
+            .characteristics = (struct ble_gatt_chr_def[])
+            {
+                {
+                    .uuid = BLE_UUID16_DECLARE(MANUFACTURER_NAME),
+                    .flags = BLE_GATT_CHR_F_READ,
+                    .access_cb = Device_Info
+                },
+                {
+                    0
+                }
+            }
+        },
 
-    {.type = BLE_GATT_SVC_TYPE_PRIMARY, .uuid = BLE_UUID16_DECLARE(DEVICE_BATTERY_SERVICE), .characteristics = (struct ble_gatt_chr_def[]){{.uuid = BLE_UUID16_DECLARE(BATTERY_INFORMATION), .flags = BLE_GATT_CHR_F_READ, .access_cb = Device_Battery_Information}, {.uuid = BLE_UUID16_DECLARE(BATTERY_LEVEL), .flags = BLE_GATT_CHR_F_READ, .access_cb = Device_Battery_Level}, {0}}},
+        {
+            .type = BLE_GATT_SVC_TYPE_PRIMARY, 
+            .uuid = BLE_UUID16_DECLARE(DEVICE_BATTERY_SERVICE), 
+            .characteristics = (struct ble_gatt_chr_def[])
+            {
+                {
+                    .uuid = BLE_UUID16_DECLARE(BATTERY_INFORMATION), 
+                    .flags = BLE_GATT_CHR_F_READ, 
+                    .access_cb = Device_Battery_Information
+                }, 
+                {
+                    .uuid = BLE_UUID16_DECLARE(BATTERY_LEVEL), 
+                    .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_NOTIFY, 
+                    .access_cb = Device_Battery_Level, 
+                    .val_handle = &Battery_level_characteristic_attribute_handler, 
+                    .descriptors = (struct ble_gatt_dsc_def[])
+                    {
+                        {
+                            .uuid = BLE_UUID16_DECLARE(BATTERY_CLIENT_CONFIG_DESCRIPTOR), 
+                            .att_flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_WRITE, 
+                            .access_cb = Battery_Level_Descriptor
+                        }, 
+                        {
+                            0
+                        }
+                    }
+                }
+            } 
+        },
 
-    {.type = BLE_GATT_SVC_TYPE_PRIMARY, .uuid = BLE_UUID128_DECLARE(0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff), .characteristics = (struct ble_gatt_chr_def[]){{.uuid = BLE_UUID128_DECLARE(0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00), .flags = BLE_GATT_CHR_F_WRITE, .access_cb = Custom_Service}, {0}}},
-
-    {0}};
+        {
+            .type = BLE_GATT_SVC_TYPE_PRIMARY, 
+            .uuid = BLE_UUID128_DECLARE(0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff), 
+            .characteristics = (struct ble_gatt_chr_def[])
+            {
+                {
+                    .uuid = BLE_UUID128_DECLARE(0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00), 
+                    .flags = BLE_GATT_CHR_F_WRITE, 
+                    .access_cb = Custom_Service
+                }, 
+                {
+                    0
+                }
+            }
+        },
+        {
+            0
+        }
+    };
 
 int BLE_gap_event(struct ble_gap_event *event, void *arg)
 {
@@ -81,6 +167,7 @@ int BLE_gap_event(struct ble_gap_event *event, void *arg)
         {
             BLE_app_advertise();
         }
+        connection_handler = event->connect.conn_handle;
         break;
 
     case BLE_GAP_EVENT_DISCONNECT:
@@ -95,6 +182,11 @@ int BLE_gap_event(struct ble_gap_event *event, void *arg)
 
     case BLE_GAP_EVENT_SUBSCRIBE:
         ESP_LOGI("GAP", "BLE_GAP_EVENT_SUBSCRIBE");
+
+        if (event->subscribe.attr_handle == Battery_level_characteristic_attribute_handler)
+        {
+            xTimerStart(Battery_Timer_Handler, 0);
+        }
         break;
 
     default:
@@ -138,6 +230,20 @@ void Host_task(void *param)
     nimble_port_run(); /* Run the nimble port */
 }
 
+void Update_Battery_Timer()
+{
+    if (Battery_Level-- == 0)
+    {
+        Battery_Level = 100;
+    }
+
+    printf("Reporting battery level %d\n", Battery_Level);
+
+    struct os_mbuf *om = ble_hs_mbuf_from_flat(&Battery_Level, sizeof(Battery_Level));
+
+    ble_gattc_notify_custom(connection_handler, Battery_level_characteristic_attribute_handler, om);
+}
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init()); /* Initialize NVS flash */
@@ -153,5 +259,8 @@ void app_main(void)
     ble_gatts_add_svcs(GATT_Service);
 
     ble_hs_cfg.sync_cb = BLE_app_on_sync; /* Set the synchronization callback */
+
+    Battery_Timer_Handler = xTimerCreate("Update_Battery_Timer", pdMS_TO_TICKS(1000), pdTRUE, NULL, Update_Battery_Timer);
+
     nimble_port_freertos_init(Host_task); /* Initialize NimBLE port with FreeRTOS */
 }
